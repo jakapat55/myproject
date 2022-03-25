bbb6a686da7806b9771a598e1fb136d98e1ee365
jakapat <jakapat@mail.kmutt.ac.th>
2022-03-25 10:22:57 +0700

    verion1

--- src/main/java/com/mycompany/myproject/myproject.java
+++ src/main/java/com/mycompany/myproject/myproject.java
@@ -5,5 +5,6 @@ package com.mycompany.myproject;
 public class myproject {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
